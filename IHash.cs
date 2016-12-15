4cf13b67ac19f76cd422f591f932eec882cedb22
A-D-A <aleksanyanda@yandex.ru>
2016-12-16 00:58:51 +0300

    MD5 added

---
+++ IHash.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CryproLib
+{
+    public interface IHash
+    {
+        void GetHash(Stream input, Stream output);
+
+    }
+}
---
+++ ISign.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CryproLib
+{
+    public interface ISign
+    {
+        void Sign(Stream input, Stream output);
+        void SetKey(Stream key);
+        void SetHashFunction(IHash hash);
+        bool Verify(Stream input);
+    }
+}
